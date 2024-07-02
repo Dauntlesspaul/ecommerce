ccc32c5348c404465ddc730d31f460c2cc1fd696
dauntless <dauntlesspaulwit@gmail.com>
2024-07-02 02:17:22 +0100

    updated cors setup

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json())
 app.use(bodyParser.json());
 const crossOrigins = [
-  'http://shoe-haven.vercel.app',
+  'https://shoe-haven.vercel.app',
 ]
 const corsOptions = {
   origin: crossOrigins,
