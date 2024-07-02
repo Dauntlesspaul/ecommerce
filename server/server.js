1f40530ab0b70eb6acbe026394c4278b979b9d01
dauntless <dauntlesspaulwit@gmail.com>
2024-07-02 23:14:35 +0100

    session storage update

--- server/server.js
+++ server/server.js
@@ -20,7 +20,9 @@ app.use(session({
      ttl: 14 * 24 * 60 * 60 
   }),
   cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 14 
+    maxAge: 1000 * 60 * 60 * 24 * 14,
+    sameSite: 'None',
+    secure: true
   }
 }));
 
