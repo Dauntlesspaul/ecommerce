d52d9bd01afaaaaeb444393c8b20a07c51a660c2
dauntless <dauntlesspaulwit@gmail.com>
2024-07-03 16:57:44 +0100

    new update

--- server/server.js
+++ server/server.js
@@ -1,49 +1,48 @@
-const express = require('express')
+const express = require('express');
 require('dotenv').config();
-const app = express()
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const session = require('express-session')
-const MongoStore = require('connect-mongo')
-const cors = require('cors')
-const {connectDB} = require('./config/db')
-const PORT = process.env.PORT || 8000
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const cors = require('cors');
+const { connectDB } = require('./config/db');
+const PORT = process.env.PORT || 8000;
+
+const app = express();
 connectDB();
 
-
-
-app.use(session({
-  secret:"keyboard mouse",
-  resave:false,
-  saveUninitialized:false,
-  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI,
-     ttl: 14 * 24 * 60 * 60 // 14 days
-  }),
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 14 // 14 days
-  }
-}));
-
-app.use('/api/stripe',require('./route'))
-app.use(express.urlencoded({extended:true}));
-app.use(express.json())
-app.use(bodyParser.json());
 const crossOrigins = [
   'https://shoe-haven.vercel.app',
-]
+];
+
 const corsOptions = {
   origin: crossOrigins,
   optionsSuccessStatus: 200,
   credentials: true 
 };
+
 app.use(cors(corsOptions));
-app.use(cookieParser())
-app.use('/',require('./route'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+app.use(session({
+  secret: "keyboard mouse",
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI, ttl: 14 * 24 * 60 * 60 }),
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 14, 
+    sameSite: 'None',
+    secure: true,
+    httpOnly: true
+  }
+}));
 
-app.listen(PORT, ()=>{
-  return   console.log(`app is running at port ${PORT}`)
-})
+// Routes
+app.use('/api/stripe', require('./route'));
+app.use('/', require('./route'));
 
-
-
+app.listen(PORT, () => {
+  console.log(`App is running at port ${PORT}`);
+});
