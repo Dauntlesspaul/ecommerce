f9e30924bbedc2390726750b79085566cbe3fd47
dauntless <dauntlesspaulwit@gmail.com>
2024-07-03 19:47:06 +0100

    update

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use(session({
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 14, // 14 days
     sameSite: 'none', 
-    secure: true,
+    secure: 'auto',
     httpOnly: true,
      domain: '.vercel.app'
   }
