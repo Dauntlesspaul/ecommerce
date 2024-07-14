483a6fbc05611b64fdfd262d3b3bb6ffc7af5d76
dauntless <dauntlesspaulwit@gmail.com>
2024-07-14 21:10:23 +0100

    update

--- server/server.js
+++ server/server.js
@@ -45,6 +45,14 @@ app.use(session({
 
 app.use('/api', require('./route'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`App is running at port ${PORT}`);
 });
